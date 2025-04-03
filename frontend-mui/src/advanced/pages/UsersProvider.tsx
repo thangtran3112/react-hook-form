87df718a0e12be972e3f2369cbb52d4a28f1e665
Toby Tran <ttran@stackline.com>
2025-04-03 09:56:06 -0700

    fix

--- frontend-mui/src/advanced/pages/UsersProvider.tsx
+++ frontend-mui/src/advanced/pages/UsersProvider.tsx
@@ -3,13 +3,13 @@ import { FormProvider, useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 import { zodResolver } from "@hookform/resolvers/zod";
 
-import { defaultValues, Schema, schema } from "../schema/schema";
+import { defaultValues, ZAdvancedUser, schema } from "../schema/schema";
 import { Users } from "./Users";
 import useZustandForm from "../../stores/formStore";
 
 export function UsersProvider() {
   const { mode } = useZustandForm();
-  const methods = useForm<Schema>({
+  const methods = useForm<ZAdvancedUser>({
     mode: mode,
     resolver: zodResolver(schema),
     defaultValues,
